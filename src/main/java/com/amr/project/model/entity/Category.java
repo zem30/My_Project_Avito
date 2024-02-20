c4fdd593b45dba98aafdcdc056002063e461a326
Alexe1Kal1n1n <kalinin.aleks199813@yandex.ru>
2024-02-20 21:19:01 +0400

    Хуйня Categories работает

--- src/main/java/com/amr/project/model/entity/Category.java
+++ src/main/java/com/amr/project/model/entity/Category.java
@@ -2,16 +2,19 @@ package com.amr.project.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.hibernate.id.GUIDGenerator;
+
 import javax.persistence.*;
 import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
 @Entity
 @Table(name = "category")
-@NoArgsConstructor
+//@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@Data
 @Builder
 public class Category {
 
@@ -20,21 +23,32 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "category_name",unique = true)
+    @Column(name = "guid", unique = true)
+    private UUID guid;
+
+    @Column(name = "category_name", unique = true)
     private String name;
 
     @JsonIgnore
-    @ManyToMany(fetch = FetchType.LAZY , cascade = CascadeType.REFRESH)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
     @JoinTable(
             name = "category_items",
-            joinColumns = { @JoinColumn(name = "category_id")},
-            inverseJoinColumns = { @JoinColumn(name = "item_id") }
+            joinColumns = {@JoinColumn(name = "category_id")},
+            inverseJoinColumns = {@JoinColumn(name = "item_id")}
     )
     private List<Item> items;
 
     @JsonIgnore
-    @ManyToMany(fetch = FetchType.LAZY , cascade = CascadeType.REFRESH)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
     @JoinTable(name = "category_advert")
     private List<Advert> adverts;
+
+    public Category() {
+        this.guid = UUID.randomUUID();
+    }
+
+    public String getGuid() {
+        return guid.toString().replace("-","").toUpperCase();
+    }
 
 }
